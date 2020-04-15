7a69cece8f6c27e1445e021106acea8e79f74014
Eddie Crawford <eddie.crawford@ibotta.com>
2020-04-15 17:01:33 -0400

    Add standard extensions Map<String, Object> to GraphError (#90)

--- library/src/main/java/io/github/wax911/library/model/attribute/GraphError.kt
+++ library/src/main/java/io/github/wax911/library/model/attribute/GraphError.kt
@@ -3,7 +3,8 @@ package io.github.wax911.library.model.attribute
 data class GraphError(
         val message: String?,
         val status: Int,
-        val locations: List<Map<String, Int>>?
+        val locations: List<Map<String, Int>>?,
+        val extensions: Map<String, Any>?
 ) {
 
     override fun toString(): String {
@@ -11,6 +12,7 @@ data class GraphError(
                 "message='" + message + '\''.toString() +
                 ", status=" + status +
                 ", locations=" + locations +
+                ", extensions=" + extensions +
                 '}'.toString()
     }
 }
