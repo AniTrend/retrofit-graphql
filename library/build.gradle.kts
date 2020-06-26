e0308c4b55209d2f312d7adde3bf3fd96f52af65
Maxwell <mxt.developer@gmail.com>
2020-06-26 22:27:54 +0200

    Add androidx.annotation artifact
    This was added to allow us to make use of some annotations such as `VisibleForTesting`

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,3 +1,9 @@
+import co.anitrend.retrofit.graphql.buildSrc.Libraries
+
 plugins {
     id("co.anitrend.retrofit.graphql")
+}
+
+dependencies {
+    implementation(Libraries.AndroidX.Annotation.annotation)
 }
