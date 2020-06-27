a1a46e6236fe86a0f0eedb9f90951225e8cc0c40
Maxwell <mxt.developer@gmail.com>
2020-06-28 00:02:58 +0200

    Add resources discovery plugin test case
    Provides an example test case for using resources rather than assets, this is a complementary case for issue: #71
    
    See commit: c6dfa1546bfebe95242ac07962d9b138bf5e08a0

---
+++ library/src/test/kotlin/io/github/wax911/library/helpers/ResourcesDiscoveryPlugin.kt
@@ -0,0 +1,59 @@
+package io.github.wax911.library.helpers
+
+import io.github.wax911.library.annotation.processor.plugin.contract.AbstractDiscoveryPlugin
+import io.github.wax911.library.logger.core.AbstractLogger
+import java.io.File
+import java.io.InputStream
+import java.net.URL
+
+class ResourcesDiscoveryPlugin(
+    self: Class<ResourcesDiscoveryPlugin> = ResourcesDiscoveryPlugin::class.java
+) : AbstractDiscoveryPlugin<Class<ResourcesDiscoveryPlugin>>(self) {
+    override val targetPath: String = "graphql"
+    override val targetExtension: String = ".graphql"
+
+    private val temporaryMap = HashMap<String, String>()
+
+    private fun getResourceFolderFiles(url: URL): List<File>? {
+        val path = url.path
+        val sequenceFiles = File(path).walk()
+        return sequenceFiles.filter {
+            val fileName = it.name
+            fileName.endsWith(targetExtension)
+        }.toList()
+    }
+
+    /**
+     * Reads the file contents for a given [inputStream]
+     */
+    override fun resolveContents(
+        inputStream: InputStream,
+        logger: AbstractLogger
+    ): String {
+        val queryBuffer = StringBuilder()
+        return runCatching {
+            inputStream.bufferedReader().useLines { sequence ->
+                sequence.forEach { queryBuffer.append(it) }
+            }
+            queryBuffer.toString()
+        }.getOrThrow()
+    }
+
+    /**
+     * Invokes initial discovery using [source] to produce a map
+     */
+    override fun startDiscovery(logger: AbstractLogger): Map<String, String> {
+        val urlPath = source.getResource(targetPath)
+        if (urlPath != null) {
+            val files = getResourceFolderFiles(urlPath)
+            files?.forEach { file ->
+                val contents = resolveContents(file.inputStream(), logger)
+                assert(contents.isNotEmpty()) {
+                    "Contents of a file that needs to be read should never be empty"
+                }
+                temporaryMap[file.name] = contents
+            }
+        }
+        return temporaryMap
+    }
+}
