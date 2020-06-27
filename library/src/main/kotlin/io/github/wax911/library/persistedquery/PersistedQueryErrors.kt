175e40c724153b4d76a04b1ac6077304aa4f1793
Maxwell <mxt.developer@gmail.com>
2020-06-28 00:24:07 +0200

    Add deprecation warning for persisted query errors
    Suggests migrating from `PersistedQueryErrors` to `AutomaticPeristedQueryErrors`, this change only brings in updated documentation and package relocation

--- library/src/main/kotlin/io/github/wax911/library/persistedquery/PersistedQueryErrors.kt
+++ library/src/main/kotlin/io/github/wax911/library/persistedquery/PersistedQueryErrors.kt
@@ -1,5 +1,12 @@
 package io.github.wax911.library.persistedquery
 
+@Deprecated(
+    "Consider migrating to AutomaticPersistedQueryErrors instead",
+    ReplaceWith(
+        "AutomaticPersistedQueryErrors",
+        "io.github.wax911.library.persisted.query.error.AutomaticPersistedQueryErrors"
+    )
+)
 object PersistedQueryErrors {
     // server does not support Automatic Persisted Queries. The client should fallback to regular query
     const val APQ_NOT_SUPPORTED_ERROR = "PersistedQueryNotSupported"
