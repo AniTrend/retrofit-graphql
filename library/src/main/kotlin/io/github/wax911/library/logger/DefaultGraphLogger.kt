3768e64912beb8c8f96e50e46a19a271429b8db1
Maxwell <mxt.developer@gmail.com>
2020-06-27 23:29:41 +0200

    Add library default logger
    Adds a default logger for the library that writes to `android.util.Log`
    
    See commit: 3202ec651c8b17c6385dc38356339d2a58414aba
    
    Also see issue: #59

---
+++ library/src/main/kotlin/io/github/wax911/library/logger/DefaultGraphLogger.kt
@@ -0,0 +1,33 @@
+package io.github.wax911.library.logger
+
+import android.util.Log
+import io.github.wax911.library.logger.core.AbstractLogger
+import io.github.wax911.library.logger.contract.ILogger.Level
+
+/**
+ * Default logger for the library that writes to [Log]
+ * with a default log [level] level of [Level.INFO]
+ */
+class DefaultGraphLogger(
+    level: Level = Level.INFO
+) : AbstractLogger(level) {
+
+    /**
+     * Write a log message to its destination.
+     *
+     * @param level Log [Level] filter
+     * @param tag Identifier log tag
+     * @param message Optional log message
+     * @param throwable Optional exception
+     */
+    override fun log(level: Level, tag: String, message: String, throwable: Throwable?) {
+        when (level) {
+            Level.VERBOSE -> Log.v(tag, message, throwable)
+            Level.DEBUG -> Log.d(tag, message, throwable)
+            Level.INFO -> Log.i(tag, message, throwable)
+            Level.WARNING -> Log.w(tag, message, throwable)
+            Level.ERROR -> Log.e(tag, message, throwable)
+            Level.NONE -> { /** no logging at none */ }
+        }
+    }
+}
