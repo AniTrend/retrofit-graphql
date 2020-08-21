3c6496d345ee97373cdb218b0126473a62c6727d
Maxwell <mxt.developer@gmail.com>
2020-08-21 11:17:36 +0200

    Bump android.gradle build tools version form 4.0.0 to 4.0.1

--- buildSrc/src/main/java/co/anitrend/retrofit/graphql/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/retrofit/graphql/buildSrc/Libraries.kt
@@ -15,7 +15,7 @@ object Libraries {
     object Android {
 
         object Tools {
-            private const val version = "4.0.0"
+            private const val version = "4.0.1"
             const val buildGradle = "com.android.tools.build:gradle:$version"
         }
     }
