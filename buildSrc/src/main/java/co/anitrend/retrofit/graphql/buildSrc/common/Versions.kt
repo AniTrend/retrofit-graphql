a43fa1774c493ef3a679cde76a65ef514fa6e9d1
Maxwell <mxt.developer@gmail.com>
2021-02-24 12:17:33 +0200

    Bump release version from 0.11.0-alpha02 to 0.11.0-alpha03

--- buildSrc/src/main/java/co/anitrend/retrofit/graphql/buildSrc/common/Versions.kt
+++ buildSrc/src/main/java/co/anitrend/retrofit/graphql/buildSrc/common/Versions.kt
@@ -6,8 +6,8 @@ object Versions {
     const val targetSdk = 29
     const val minSdk = 17
 
-    const val versionCode = 11002
-    const val versionName = "0.11.0-alpha02"
+    const val versionCode = 11003
+    const val versionName = "0.11.0-alpha03"
 
     const val mockk = "1.10.0"
     const val junit = "4.13"
