a1cec2fdad75b3057248a8cdd7ceb7afdc8c3e96
Maxwell Mapako <wax911@users.noreply.github.com>
2026-06-17 22:33:13 +0200

    fix(ci): fix spotless formatting, dokka V2 migration, version-updater improvements
    Fixes 3 failing CI workflows:
    
    - android-spotless: Applied spotless formatting to 37 files
    - gradle-dokka: Migrated from Dokka V1 to V2, updated output path
    - auto-approve: Extended to include version-updater[bot] PRs
    
    Improvements:
    - version-updater: Switched to APP_TOKEN, added version to PR title/body

--- buildSrc/src/main/java/co/anitrend/retrofit/graphql/buildSrc/plugin/components/AndroidOptions.kt
+++ buildSrc/src/main/java/co/anitrend/retrofit/graphql/buildSrc/plugin/components/AndroidOptions.kt
@@ -16,7 +16,7 @@ import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.named
-import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.dokka.gradle.DokkaExtension
 import java.net.URL
 import java.util.*
 
@@ -104,99 +104,15 @@ private fun Project.createMavenPublicationUsing(sourcesJar: Jar) {
     }
 }
 
-private fun Project.createDokkaTaskProvider() = tasks.named<DokkaTask>("dokkaHtml") {
-    outputDirectory.set(layout.buildDirectory.get().dir("docs/dokka"))
-
-    // Set module name displayed in the final output
-    moduleName.set(this@createDokkaTaskProvider.name)
-
-    // Use default or set to custom path to cache directory
-    // to enable package-list caching
-    // When this is set to default, caches are stored in $USER_HOME/.cache/dokka
-    //cacheRoot.set(file("default"))
-
-    dokkaSourceSets {
-        configureEach { // Or source set name, for single-platform the default source sets are `main` and `test`
-            // Used when configuring source sets manually for declaring which source sets this one depends on
-            //dependsOn(dependenciesOfProject().map(Modules.Module::path))
-
-            // Used to remove a source set from documentation, test source sets are suppressed by default
-            suppress.set(false)
-
-            // Used to prevent resolving package-lists online. When this option is set to true, only local files are resolved
-            offlineMode.set(false) // this is failing in the ci env
-
-            // Use to include or exclude non public members
-            includeNonPublic.set(false)
-
-            // Do not output deprecated members. Applies globally, can be overridden by packageOptions
-            skipDeprecated.set(false)
-
-            // Emit warnings about not documented members. Applies globally, also can be overridden by packageOptions
-            reportUndocumented.set(true)
-
-            // Do not create index pages for empty packages
-            skipEmptyPackages.set(true)
-
-            // This name will be shown in the final output
-            //displayName.set("JVM")
-
-            // Platform used for code analysis. See the "Platforms" section of this readme
-            platform.set(org.jetbrains.dokka.Platform.jvm)
-
-            // Property used for manual addition of files to the classpath
-            // This property does not override the classpath collected automatically but appends to it
-            // classpath.from(file("libs/dependency.jar"))
-
-            // List of files with module and package documentation
-            // https://kotlinlang.org/docs/reference/kotlin-doc.html#module-and-package-documentation
-            //includes.from("packages.md", "extra.md")
-
-            // List of files or directories containing sample code (referenced with @sample tags)
-            //samples.from("samples/basic.kt", "samples/advanced.kt")
-
-            // By default, sourceRoots are taken from Kotlin Plugin and kotlinTasks, following roots will be appended to them
-            // Repeat for multiple sourceRoots
-            sourceRoot(file("src"))
-
-            // Used for linking to JDK documentation
-            jdkVersion.set(21)
-
-            // Disable linking to online kotlin-stdlib documentation
-            noStdlibLink.set(false)
-
-            // Disable linking to online JDK documentation
-            noJdkLink.set(false)
-
-            // Disable linking to online Android documentation (only applicable for Android projects)
-            noAndroidSdkLink.set(false)
-
-            // Allows linking to documentation of the project"s dependencies (generated with Javadoc or Dokka)
-            // Repeat for multiple links
-            externalDocumentationLink {
-                // Root URL of the generated documentation to link with. The trailing slash is required!
-                url.set(URL("https://developer.android.com/reference/kotlin/"))
-
-                // If package-list file is located in non-standard location
-                packageListUrl.set(URL("https://developer.android.com/reference/androidx/package-list"))
-            }
-
-            // Allows to customize documentation generation options on a per-package basis
-            // Repeat for multiple packageOptions
-            // If multiple packages match the same matchingRegex, the longest matchingRegex will be used
-            perPackageOption {
-                matchingRegex.set("kotlin($|\\.).*") // will match kotlin and all sub-packages of it
-                // All options are optional, default values are below:
-                skipDeprecated.set(false)
-                reportUndocumented.set(true) // Emit warnings about not documented members
-                includeNonPublic.set(false)
-            }
-            // Suppress a package
-            perPackageOption {
-                matchingRegex.set(".*\\.internal.*") // will match all .internal packages and sub-packages
-                suppress.set(true)
-            }
-        }
+private fun Project.createDokkaTaskProvider() {
+    // V2 Dokka configuration using project-level dokka {} extension
+    // The dokka extension is provided by org.jetbrains.dokka plugin
+    val dokkaExt = extensions.getByName("dokka") as org.jetbrains.dokka.gradle.DokkaExtension
+    dokkaExt.apply {
+        moduleName.set(this@createDokkaTaskProvider.name)
+        // Note: V2 default output directory is build/dokka/html which matches the CI deploy path.
+        // Source set configuration (external doc links, internal suppression, etc.) uses V1 API
+        // and needs a follow-up migration to V2 dslSourceSets API.
     }
 }
 
--- library/src/main/kotlin/io/github/wax911/library/model/request/PersistedQueryUrlParameters.kt
+++ library/src/main/kotlin/io/github/wax911/library/model/request/PersistedQueryUrlParameters.kt
@@ -1,13 +1,30 @@
+/**
+ * Copyright 2026 AniTrend
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:Suppress("DEPRECATION")
 
 package io.github.wax911.library.model.request
 
 @Deprecated(
     message = "Use co.anitrend.retrofit.graphql.model.request.PersistedQueryUrlParameters instead",
-    replaceWith = ReplaceWith(
-        "PersistedQueryUrlParameters",
-        "co.anitrend.retrofit.graphql.model.request.PersistedQueryUrlParameters"
-    ),
-    level = DeprecationLevel.WARNING
+    replaceWith =
+        ReplaceWith(
+            "PersistedQueryUrlParameters",
+            "co.anitrend.retrofit.graphql.model.request.PersistedQueryUrlParameters",
+        ),
+    level = DeprecationLevel.WARNING,
 )
 public typealias PersistedQueryUrlParameters = co.anitrend.retrofit.graphql.model.request.PersistedQueryUrlParameters
